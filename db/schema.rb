ee932474d1902035a26c4f8e26adf8d51edacccc
hramli <ramliharry48@yahoo.com>
2019-10-23 17:49:26 +0000

    added foreign key of users to Listings table for relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191023173728) do
+ActiveRecord::Schema.define(version: 20191023174758) do
 
   create_table "listings", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20191023173728) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
