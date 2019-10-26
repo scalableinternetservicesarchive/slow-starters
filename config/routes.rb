ab4244d73240c9414012197888a23fa83afc7df9
hramli <ramliharry48@yahoo.com>
2019-10-26 19:46:49 +0000

    override registration controller for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :listings
   resources :users, only: [:show]
   root "listings#index"
