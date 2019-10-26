fa3700eb4544796361b597228858bf8ddd97d796
hramli <ramliharry48@yahoo.com>
2019-10-26 21:38:42 +0000

    updated listing view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, length: { maximum: 20 }
   validates_uniqueness_of :username #handles duplicate username
   has_many :listings         
 end
