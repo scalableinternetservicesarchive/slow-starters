1fa00b7c7b3d4a061a25ebf7bd77f71d5937803a
Harry Ramli <ramliharry48@yahoo.com>
2019-11-17 21:27:03 -0800

    added search feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:show]
   root "listings#index"
+  get 'search', to:'listings#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
