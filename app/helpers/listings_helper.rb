520ea941a2fa18a7cb5641e9996798d3520007f7
Harry Ramli <ramliharry48@yahoo.com>
2019-11-25 15:11:51 -0800

    seed + server-side caching cleanup

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -2,4 +2,8 @@ module ListingsHelper
     def cache_key_for_listing_card(listing)
         "listing-#{listing.id}-#{listing.updated_at}"
     end
+
+    def cache_key_for_listing
+        "listings-page-#{Listing.maximum(:updated_at)}-#{Message.maximum(:updated_at)}-#{Reservation.maximum(:updated_at)}"
+    end
 end
