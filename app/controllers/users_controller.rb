b1da00b42755d986f021d7f62fd5e3f994648c99
Michael Wang <michaelwang0213@yahoo.com>
2019-11-25 20:54:33 +0000

    added partial for profile card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,41 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @seller_score = getSellerRating
+    @buyer_score = getBuyerRating
   end
+  
+  private
+    
+    def getSellerRating
+      @user = User.find(params[:id])
+      scores = []
+      @user.listings.each { |listing| 
+        listing.reservations.each { |reservation| 
+          listing_ratings = reservation.ratings.find_all { |rating| rating.from_seller == false }
+          listing_ratings.each { |rating| scores.push rating.score }
+        }
+      }
+      score = scores.inject{ |sum, el| sum + el }.to_f / scores.size
+      if score.to_f.nan? 
+        score = 0
+      end
+
+      return score
+    end
+    
+    def getBuyerRating
+      @user = User.find(params[:id])
+      scores = []
+      @user.reservations.each { |reservation| 
+        reservation_ratings = reservation.ratings.find_all { |rating| rating.from_seller == true }
+        reservation_ratings.each { |rating| scores.push rating.score }
+      }
+      score = scores.inject{ |sum, el| sum + el }.to_f / scores.size
+      if score.to_f.nan? 
+        score = 0
+      end
+
+      return score
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :username, presence: true, length: { maximum: 20 }
   validates_uniqueness_of :username #handles duplicate username
-  has_many :listings         
+  has_many :listings      
+  has_many :reservations
 end
