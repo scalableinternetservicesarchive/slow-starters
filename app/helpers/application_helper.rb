c56823cd9dac32c4a7c1b1c59e1ec05b5668f24e
Michael Wang <michaelwang0213@yahoo.com>
2019-12-04 16:54:53 +0000

    some styling changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+  def listingStatus(listing, user)
+    if not user_signed_in?
+      return "Log in to Reserve", ""
+    elsif listing.user_id == user.id
+      return "Owner", "green"
+    else
+      reservation = listing.reservations.find{|reservation| reservation.user_id == current_user.id}
+      if reservation == nil
+        return "Reserve Now!", ""
+      else
+        if reservation.seller_confirmed && reservation.buyer_confirmed
+          return "Completed", "#DAA520"
+        else
+          return "Reserved", "green"
+        end
+      end
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,22 +17,23 @@ listings = []
         password_confirmation: '123456')
     users.push(u.id)
     
-    
-    
     50.times do |n|
         minute = rand(0...60)
         minute = format('%02d', minute)
         second = rand(0...60)
         second = format('%02d', second)
-        
+        location = rand(1...8)
+        price = rand(5... 11)
         l = Listing.create!(
             user_id: u.id,
             title: 'hi', 
             price: 3, 
+            price: price, 
             available_swipes: 100,
             location: 1,
+            location: location,
             time: "2019-11-30T08:" + minute + ":" + second,
             description: 'test')
         listings.push(l.id)
-    end	
+    end
 end
