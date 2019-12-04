df5fd93f70287b42cbcdedb4e0bed7b75814f948
Harry Ramli <ramliharry48@yahoo.com>
2019-12-04 14:24:26 -0800

    removed threads

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 3)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+workers 5
+threads_count = 5
 threads threads_count, threads_count
 
 preload_app!
